b6924b46aa5f3a9122c9bb0e90c15b9a31ab5d46
keith <keithcicala123@gmail.com>
2022-04-29 18:07:52 -0400

    fixed errors in user_test

--- tests/user_test.py
+++ tests/user_test.py
@@ -38,6 +38,7 @@ def test_adding_user(application):
         assert song2.title == "SuperSongTitle"
         #checking cascade delete
         db.session.delete(user)
+        db.session.delete(song1,song2)
         assert db.session.query(User).count() == 0
         assert db.session.query(Song).count() == 0
 
